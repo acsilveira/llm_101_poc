1d2b1d7f7d210902e8f4da1c0dc00711aae33ded
acsilveira <andrecarvalhosilveira@gmail.com>
2024-05-18 19:56:33 -0300

    Move workflow to controller class

--- src/controller_llm.py
+++ src/controller_llm.py
@@ -12,6 +12,7 @@ class ControllerLlm:
     def __init__(self, url, question):
         self._url = url
         self._question = question
+        self._model_choice = None
         self._utils = toolkit.UtilsLLM()
         self._vector_store_client = None
         self._chain = None
@@ -27,6 +28,10 @@ class ControllerLlm:
     @property
     def question(self):
         return self._question
+
+    @property
+    def model_choice(self):
+        return self._model_choice
 
     @property
     def utils(self):
@@ -55,6 +60,10 @@ class ControllerLlm:
     @question.setter
     def question(self, value):
         self._question = value
+
+    @model_choice.setter
+    def model_choice(self, value):
+        self._model_choice = value
 
     @utils.setter
     def utils(self, value):
@@ -155,6 +164,62 @@ class ControllerLlm:
         # self._utils.log("Question asked and answer received.")
         pass
 
+    def ask_to_llm(self):
+        """ Ask a question about a content to a LLM model """
+
+        # Authenticate
+        _, log_msg = self.authenticate()
+
+        # Prepare content
+        return_success, log_msg, vector_store_loaded_client = self.prepare_content_in_vector_store()
+        if return_success < 0:
+            return return_success, log_msg
+
+        # Prepare LLM chain
+        self.prepare_LLM_chain(vector_store_loaded_client)
+
+        # Ask question about the content
+        _, answer = self.ask_question_to_llm(self.model_choice)
+        return 1, answer
+
+    def prepare_content_in_vector_store(self):
+        """ Get content, apply embedding and save vectors in a vector store """
+
+        # Get text content
+        text_content, log_msg = self.get_content(mode="text_no_parse")
+        if not text_content:
+            return -1, "Content not accessible", None
+
+        _, log_msg = self.describe_chunks(text_content)
+
+        # Define embedding model
+        embedding_model, log_msg = self.define_embedding_model()
+
+        # Create/reset vector store index
+        _, log_msg = self.create_vector_store_index()
+
+        # Upload vectors to vector store
+        (
+            vector_store_loaded_client,
+            log_msg,
+        ) = self.upload_vectors_to_vector_store(
+            text_content, embedding_model
+        )
+
+        # Wait some time, to have vectorstore available
+        self._utils.wait_for(
+            seconds_to_wait=general_parameters.par__waiting_time_in_seconds
+        )
+
+        # Check if the new index exists
+        result_success, log_msg = self.check_if_vector_store_index_exists()
+        if not result_success:
+            return -2, "Namespace does not exist in vector store", None
+
+        # Check availability of the vectorestore
+        # ToDo
+        return 1, log_msg, vector_store_loaded_client
+
     def split_documents_into_chunks(
         self, documents_content, parr_chunk_size=500, parr_chunk_overlap=50
     ):
@@ -170,7 +235,7 @@ class ControllerLlm:
             raise e
 
     def authenticate(self):
-        """ Do the authentications needed """
+        """ Authenticate APIs """
 
         try:
             self.logger.info(f"Env vars loaded: {load_dotenv()}")  # Check env_vars
@@ -194,17 +259,12 @@ class ControllerLlm:
             raise e
 
     def ask_question_to_llm(self, llm_model_choice):
-        if llm_model_choice == "Gemini":
-            answer, log_msg = self._utils.asking_question_about_content_gemini(
-                self._chain, self._question
-            )
-        elif llm_model_choice == "chatGPT":
-            answer, log_msg = self._utils.asking_question_about_content_chat_gpt(
-                self._chain, self._question
-            )
-        else:
-            self.logger.error("Unknown LLM model choice")
-            return False, "Unknown LLM model choice"
+        """ Ask a question to a LLM model """
+
+        answer, log_msg = self._utils.asking_question_about_content_using_retrieved_documents_chain(
+            self._chain, self._question
+        )
+        self.logger.info(f"Question asked to {llm_model_choice}")
         self.logger.info(f"Q: {self._question}")
         self.logger.info("A: " + answer["answer"])
         self.logger.info(answer)
@@ -240,47 +300,74 @@ class ControllerLlm:
         return None, log_msg
 
     def define_embedding_model(self):
-        embedding_model, log_msg = self._utils.define_embedding_model()
+        """ Define the embedding model to be used to transform the content in vectors """
+
+        embedding_model, log_msg = self._utils.define_embedding_model_google()
         return embedding_model, log_msg
 
     def create_vector_store_index(self):
+        """ Create a namespace in a vector database """
+
         _, log_msg = self._utils.create_pinecone_index(
             self._vector_store_client, general_parameters.par__vector_store_index_name
         )
         return None, log_msg
 
     def upload_vectors_to_vector_store(self, chunks, embedding_model):
-        vectorstore_from_docs, log_msg = self._utils.upload_vectors_to_vectorstore(
+        """ Upload vectors of the content embedded to a vector database """
+
+        vectorstore_loaded, log_msg = self._utils.upload_vectors_to_pinecone(
             general_parameters.par__vector_store_index_name, chunks, embedding_model,
         )
-        return vectorstore_from_docs, log_msg
+        return vectorstore_loaded, log_msg
 
     def check_if_vector_store_index_exists(self):
-        _, log_msg = self._utils.check_if_pinecone_index_exists(
+        """ Check if a namespace exists in a vector database """
+
+        result_success, log_msg = self._utils.check_if_pinecone_index_exists(
             self._vector_store_client, general_parameters.par__vector_store_index_name
         )
-        return _, log_msg
+        return result_success, log_msg
 
     def define_llm_model(self, llm_model_choice):
-        llm_model, log_msg = self._utils.define_llm_model(llm_model_choice)
+        """ Define a llm model to be asked """
+
+        if llm_model_choice == "Gemini":
+            llm_model, log_msg = self._utils.define_llm_model_google()
+        elif llm_model_choice == "chatGPT":
+            llm_model, log_msg = self._utils.define_llm_model_openai()
+        else:
+            raise Exception("Unknown model choice")
         return llm_model, log_msg
 
     def prepare_prompt(self):
-        prompt, log_msg = self._utils.prepare_prompt()
+        """ Prepare the prompt for asking to LLM """
+
+        prompt, log_msg = self._utils.prepare_prompt_with_vector_store()
         return prompt, log_msg
 
-    def build_chain(self, vectorstore_from_docs, llm_model, prompt, llm_model_choice):
-        if llm_model_choice == "Gemini":
-            self._chain, log_msg = self._utils.build_chain_gemini(
-                vectorstore_from_docs, llm_model, prompt
-            )
-            self.chain_ready = True
-        elif llm_model_choice == "chatGPT":
-            self._chain, log_msg = self._utils.build_chain_chat_gpt(
-                vectorstore_from_docs, llm_model, prompt
-            )
-            self.chain_ready = True
-        else:
-            log_msg = "Unknown LLM model choice."
-            self.logger.error(log_msg)
+    def build_chain(self, vector_store_loaded_client, llm_model, prompt):
+        """ Build the LLM chain to ask a question to the LLM model """
+
+        self._chain, log_msg = self._utils.build_retrieved_documents_chain(
+            vector_store_loaded_client, llm_model, prompt
+        )
+        self.chain_ready = True
         return None, log_msg
+
+    def prepare_LLM_chain(self, vector_store_loaded_client):
+        """ Prepare LLM model, prompt and chain """
+
+        # Define LLM model
+        llm_model, log_msg = self.define_llm_model(self.model_choice)
+
+        # Prepare prompt
+        prompt, log_msg = self.prepare_prompt()
+
+        # Build chain
+        _, log_msg = self.build_chain(
+            vector_store_loaded_client,
+            llm_model,
+            prompt
+        )
+        pass
